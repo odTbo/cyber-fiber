e0c60498049d6dc972c7d0e5befbf609be3d6d2f
Vladimir <vladimirpolak2@gmail.com>
2021-07-07 11:49:58 +0200

    CREATE login/new-post/register TODO start defining forms

--- app.py
+++ app.py
@@ -12,5 +12,21 @@ def index():
 def feed_page():
     return render_template("feed.html")
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/new-post')
+def new_post():
+    return render_template('new_post.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
