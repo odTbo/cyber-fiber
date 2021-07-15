12836c9bc6f3233689dd4e9c05e0f29d9c87832e
Vladimir <vladimirpolak2@gmail.com>
2021-07-15 14:44:08 +0200

    POST CREATION DEPLOYED HEROKU TODO COMMENTS

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ else:
 
 # CONNECT DB
 if os.environ.get("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
