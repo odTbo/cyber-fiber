3f18580e4d0f88582a20fece5be95fa0a8adeda3
Vladimir <htcvlado@gmail.com>
2021-08-04 19:01:13 +0200

    ADD PROFILE PAGE

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, URL, Email, Length
 class RegisterForm(FlaskForm):
     email = StringField("E-mail*", validators=[DataRequired(), Email(), Length(min=7, max=30)])
     age = StringField("Age*", validators=[DataRequired(), Length(min=1, max=3)])
-    password = PasswordField("Password (min. 8 characters)", validators=[DataRequired(), Length(min=8, max=40)])
+    password = PasswordField("Password (min. 6 characters)", validators=[DataRequired(), Length(min=6, max=40)])
     password_confirm = PasswordField("Confirm Password (min. 8 characters)", validators=[DataRequired(), Length(min=8, max=40)])
     submit = SubmitField("Submit")
 
